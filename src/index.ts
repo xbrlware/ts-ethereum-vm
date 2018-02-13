cc084732d8adef63380a3c4e0d3246a331b4c5b0
Noah <noahingham@protonmail.com>
2018-02-13 21:03:56 +1100

    Added loading binary from file

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,21 @@
 
 import { run } from './run/run';
 import { State } from './state/state';
+import * as fs from 'fs';
 
-const main = (input: string) => {
-  const binary = Buffer.from(input, 'hex');
-  const initialState = new State().loadCode(binary);
-  console.log(`Running code: ${binary.toString('hex')}`);
+const main = (input: string | Buffer, callData: Buffer, log: boolean = true): State => {
+  if (typeof input === 'string') {
+    input = Buffer.from(input, 'hex');
+  }
+  const initialState = new State().loadCode(input).setCallData(callData);
+  console.log(`Running code: ${input.toString('hex')}`);
   console.log(`START => \t{${initialState}}`);
-  run(initialState, true);
+  return run(initialState, log);
 };
 
 // Replace with input from CLI
-// main('60606040523415600e57600080fd5b5b60016000819055505b5b60368060266000396000f30060606040525b600080fd00a165627a7a72305820af3193f6fd31031a0e0d2de1ad2c27352b1ce081b4f3c92b5650ca4dd542bb770029');
-main('60606040523415600e57600080fd5b5b603680601c6000396000f30060606040525b600080fd00a165627a7a723058209747525da0f525f1132dde30c8276ec70c4786d4b08a798eda3c8314bf796cc30029')
+fs.readFile('./src/test/erc20.bin', 'utf8', function(err: any, data: string) {
+  const program: Buffer = main(data, null, false).returnValue;
+  const callData = Buffer.from('0xa9059cbb000000000000000000000000d2feed64d23115162723f1bbd97770190a1f4ffb0000000000000000000000000000000000000000000001d234f9a33f47600000', 'hex');
+  main(program, callData, true);
+});
